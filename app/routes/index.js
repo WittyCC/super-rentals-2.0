3dfc458dd1d6ebcdb6f0efa80d75910d1f6ac0f0
Guest User <Guest@epicodus-6.local>
2017-08-08 15:28:58 -0700

    add reviews that belong to rentals

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,16 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('rental');
+    return Ember.RSVP.hash({
+      rentals: this.store.findAll('rental'),
+      reviews: this.store.findAll('review')
+    });
   },
-
   actions: {
     saveRental3(params) {
       var newRental = this.store.createRecord('rental', params);
-      newRental.save()
-        .catch(e => {
-          console.log(e.errors);
-        });
+      newRental.save();
       this.transitionTo('index');
     }
   }
